91d0847e7d3544f52de95bf4216d0885ec52743a
joa <xingyu.znou@gmail.com>
2022-09-22 10:10:03 +0900

    helm version 2.5.1

--- platform/terraform/cluster/main.tf
+++ platform/terraform/cluster/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "= 2.5.1"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-1"
 }
