09f08ce331b6bc994dab156fadf387df34fba08b
xingyu.zhou <xingyu.zhou@kinto-mobility.biz>
2022-09-22 12:54:42 +0900

    change eks module to version 17.24.0

--- platform/terraform/cluster/main.tf
+++ platform/terraform/cluster/main.tf
@@ -1,25 +1,10 @@
-terraform {
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "= 2.5.1"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-northeast-1"
-}
-
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
 
-#data "aws_eks_cluster_auth" "cluster" {
-#  name = module.eks.cluster_id
-#}
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_id
+}
 
 variable "cluster_name" {
   default = "my-cluster"
@@ -32,14 +17,7 @@ variable "instance_type" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
-    command     = "aws"
-  }
-#  token                  = data.aws_eks_cluster_auth.cluster.token
-#  load_config_file       = false
-#  version                = "~> 1.11"
+  token                  = data.aws_eks_cluster_auth.eks.token
 }
 
 data "aws_availability_zones" "available" {
@@ -48,12 +26,12 @@ data "aws_availability_zones" "available" {
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.2"
-
   name                 = "k8s-${var.cluster_name}-vpc"
   cidr                 = "172.16.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
   public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
@@ -71,38 +49,23 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "18.26.6"
-  #worker_ami_name_filter_windows = "*"
+  version = "17.24.0"
 
   cluster_name    = "eks-${var.cluster_name}"
-  cluster_version = "1.23"
+  cluster_version = "1.21"
 
   vpc_id = module.vpc.vpc_id
-  subnet_ids         = module.vpc.private_subnets
+  subnets         = module.vpc.private_subnets
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-    attach_cluster_primary_security_group = true
-
-    # Disabling and using externally provided security groups
-    create_security_group = false
-  }
-
-  eks_managed_node_groups = {
-    first = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-
+  worker_groups = [{
+      asg_max_size  = 5
       instance_type = var.instance_type
-    }
-  }
+  }]
 
   #write_kubeconfig   = true
   #config_output_path = "./"
 
-  #workers_additional_policies = [aws_iam_policy.worker_policy.arn]
+  workers_additional_policies = [aws_iam_policy.worker_policy.arn]
 }
 
 resource "aws_iam_policy" "worker_policy" {
--- platform/terraform/main.tf
+++ platform/terraform/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_version = ">= 0.14.0"
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "= 2.0.3"
+    }
+  }
+}
+
+provider "aws" {
+  #region = "ap-northeast-1"
+  region = "us-west-2"
+}
+
 module "dev_cluster" {
   source        = "./cluster"
   cluster_name  = "dev"
